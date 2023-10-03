df5b68c9f0f04638fb57d9371d8c9e7699b45143
harris.yi <harris.yi@kakaostyle.com>
2023-10-03 21:34:28 +0900

    feat: First commit for harris blog

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -6,7 +6,7 @@ const siteMetadata = {
   description: 'Welcome to the Harris Blog',
   language: 'ko-kr',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
+  siteUrl: 'https://blog-rho-orpin-92.vercel.app',
   siteRepo: 'https://github.com/harrisleesh/blog',
   siteLogo: '/static/images/logo.png',
   socialBanner: '/static/images/twitter-card.png',
