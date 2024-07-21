6420825aaf262f29aec0fecb99cf5a736f53deb4
Harris Seonghun Lee <harris@daangn.com>
2024-07-21 14:06:05 +0900

    renew blog

---
+++ src/config/const.ts
@@ -0,0 +1,6 @@
+export const baseDomain = 'https://www.seonghunlee.com';
+
+export const blogName = 'harris blog';
+export const blogDesc = '주간 이성훈';
+
+export const blogThumbnailURL = `${baseDomain}/weekly_harris.png`;
