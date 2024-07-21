c37f9a335bb4cec6605d41cb15a75e8ddfe598ad
Harris Seonghun Lee <harris@daangn.com>
2024-07-21 22:59:58 +0900

    renew blog third commit

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -6,26 +6,10 @@ import IconLinkedin from '@/components/icon/LinkedIn';
 export const Footer = () => {
   return (
     <footer className='flex flex-col items-center justify-center gap-4 pb-16 pt-20 text-center'>
-      <div className='flex justify-center gap-4'>
-        <Link href='https://github.com/harrisleesh' target='_blank'>
-          <IconGithub
-            className='fill-foreground transition hover:fill-pink-600'
-            height={30}
-            width={30}
-          />
-        </Link>
-        <Link href='https://www.linkedin.com/in/harris-lee-4947ba238' target='_blank'>
-          <IconLinkedin
-            className='fill-foreground transition hover:fill-pink-600'
-            height={30}
-            width={30}
-          />
-        </Link>
-      </div>
       <a href='https://hits.sh/www.seonghunlee.com/blog/'>
         <img
           alt='Hits'
-          src='https://hits.sh/www.seonghunlee.com/blog.svg?view=today-total&style=for-the-badge&label=visitors&color=7a583a&labelColor=7a583a'
+          src='https://hits.sh/www.seonghunlee.com/blog.svg?view=today-total&style=for-the-badge&&color=0891b2'
         />
       </a>
       <div>
