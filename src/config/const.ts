fa2d9426de84dcd13342973ad35cdf2dbdfad967
Harris Seonghun Lee <harris@daangn.com>
2024-07-28 17:37:41 +0900

    change favicon

--- src/config/const.ts
+++ src/config/const.ts
@@ -1,6 +1,6 @@
 export const baseDomain = 'https://www.seonghunlee.com';
 
-export const blogName = 'harris blog';
+export const blogName = 'Weekly Harris';
 export const blogDesc = '주간 이성훈';
 
 export const blogThumbnailURL = `${baseDomain}/weekly_harris.png`;
